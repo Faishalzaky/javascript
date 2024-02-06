5381fc2c57a1b6728c8ee32f5696a89166eb70a4
Muhammad Faishal Zaky <faishalzaky2000@gmail.com>
2024-02-06 16:08:33 +0700

    add tugas2.js

---
+++ tugas2.js/tugas2.js
@@ -0,0 +1,13 @@
+var number = 100;
+var string = "Faishal Zaky";
+var boolean = true;
+var kosong = null;
+var tidakada;
+var angka = [10,20,30,40,50];
+
+console.log(number)
+console.log(string)
+console.log(boolean)
+console.log(kosong)
+console.log(tidakada)
+console.log(angka)
