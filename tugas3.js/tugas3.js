d7a26903c0d7147e84861b3f112ccce7578de117
Muhammad Faishal Zaky <faishalzaky2000@gmail.com>
2024-02-08 10:05:06 +0700

    add tugas 3 js

---
+++ tugas3.js/tugas3.js
@@ -0,0 +1,12 @@
+function minta(x) {
+  console.log(x)
+}
+minta("berhasil");
+
+
+function mintadata() {
+  const lengkap = "Diterima";
+  return lengkap;
+}
+var tangkap = mintadata();
+console.log(tangkap);
