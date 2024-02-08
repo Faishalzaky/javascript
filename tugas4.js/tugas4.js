5985bf02bc1c688692e7bdf7ca48fad92d5a474b
Muhammad Faishal Zaky <faishalzaky2000@gmail.com>
2024-02-08 15:17:05 +0700

    add tugas 4 js

---
+++ tugas4.js/tugas4.js
@@ -0,0 +1,10 @@
+var tinggi = 90;
+
+if (tinggi > 100) {
+  console.log("Tinggi");
+} else if (tinggi === 100) {
+  console.log("Sedang");
+} else {
+  console.log("Pendek");
+}
+console.log(tinggi);
