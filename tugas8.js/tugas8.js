486398f55a30972fd911f496d5e2d626545e4fa2
Muhammad Faishal Zaky <faishalzaky2000@gmail.com>
2024-02-09 09:37:53 +0700

    add tugas 8 js

---
+++ tugas8.js/tugas8.js
@@ -0,0 +1,12 @@
+function panggilObjects() {
+  var rumah = {
+    tingkat : 3,
+    luas : 1000,
+    panjang : 600,
+    lebar : 400,
+    warna : 'maroon',
+  }
+  rumah.warna = 'merah';
+  console.log(rumah)
+}
+panggilObjects()
