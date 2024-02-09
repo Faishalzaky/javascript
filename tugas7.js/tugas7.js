4e203895e2188dbc14c355daeaeea3f5831afbc0
Muhammad Faishal Zaky <faishalzaky2000@gmail.com>
2024-02-09 09:06:19 +0700

    add tugas 7 js

---
+++ tugas7.js/tugas7.js
@@ -0,0 +1,9 @@
+var nilai1 = [10,20,30,40,50,60,70,80,90,100];
+for(let angka of nilai1){
+  console.log(angka);
+}
+
+var nilai = [10,20,30,40,50,60,70,80,90,100];
+for(var i =10; i<nilai.length; i++){
+  console.log(nilai[i]);
+}
