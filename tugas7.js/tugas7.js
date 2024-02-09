3f47073a092be8f577a54bb04c68f7db2c295079
Muhammad Faishal Zaky <faishalzaky2000@gmail.com>
2024-02-09 09:08:34 +0700

    add menambahkan comen

--- tugas7.js/tugas7.js
+++ tugas7.js/tugas7.js
@@ -1,8 +1,10 @@
+// Perulangan for of
 var nilai1 = [10,20,30,40,50,60,70,80,90,100];
 for(let angka of nilai1){
   console.log(angka);
 }
 
+// Perulangan biasa 
 var nilai = [10,20,30,40,50,60,70,80,90,100];
 for(var i =10; i<nilai.length; i++){
   console.log(nilai[i]);
