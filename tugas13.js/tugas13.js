16ea8b1f94c8c9e50734ad9f7b11b5f053b2360c
Muhammad Faishal Zaky <faishalzaky2000@gmail.com>
2024-02-18 09:51:24 +0700

    add tugas 13 js

---
+++ tugas13.js/tugas13.js
@@ -0,0 +1,10 @@
+function buah_buahan() {
+  var buah = ["Pisang", "Jeruk"];
+  console.log(buah)
+  // menambahkan sebuah string didepan :
+  buah.unshift("Apel","Mangga")
+
+  return buah
+}
+
+console.log(buah_buahan())
