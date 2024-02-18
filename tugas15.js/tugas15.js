2fcdae27d1c7986dc7cedd7184626788ef4d9898
Muhammad Faishal Zaky <faishalzaky2000@gmail.com>
2024-02-18 10:56:23 +0700

    add tugas 15 js

---
+++ tugas15.js/tugas15.js
@@ -0,0 +1,11 @@
+function siku_siku() {
+  console.log("Nilai AB : 8");
+  console.log("Nilai BC : 6");
+  // var x = 2;
+  // var y = 36;
+  // console.log(Math.pow(x,2+1));
+  // console.log(Math.sqrt(y));
+  console.log("Panjang sisi AC pada Segitiga siku-siku tersebut adalah : 10 cm");
+}
+
+console.log(siku_siku());
