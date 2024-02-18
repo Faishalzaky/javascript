b1a73a543867d5cd4639a332de5327b2eb2292aa
Muhammad Faishal Zaky <faishalzaky2000@gmail.com>
2024-02-18 14:58:32 +0700

    add tugas 17 js

---
+++ tugas17.js/tugas17.js
@@ -0,0 +1,11 @@
+function tugas17() {
+  var kalimat1 = "sAYA BElAJAR BAHAsA PEmROGRAMAN DEnGAN KHuSUK.";
+  console.log(kalimat1.toLowerCase());
+  console.log(kalimat1.toUpperCase());
+  var kalimat2 = "Allen sedang memancing ikan di sungai, dia mendapatkan ikan gabus 3 ekor dan langsung dijual.";
+  console.log(kalimat2.toLowerCase());
+  console.log(kalimat2.toUpperCase());
+  
+
+}
+tugas17();
